93400f47e21432ff145e7ad9b0914872d99f6270
Viktor_Bellavin <viktor_bellavin@epam.com>
2019-03-29 17:16:18 +0300

    Gradle build: removed unused deps in buildSrc, upgraded wrapper to version 5.3

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.gradle.kotlin.kotlin-dsl") version "1.1.3"
+    `kotlin-dsl`
 }
 
 repositories {
@@ -7,11 +7,8 @@ repositories {
 }
 val kotlinVersion = "1.3.21"
 dependencies {
-    compileOnly(gradleKotlinDsl())
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation(kotlin("stdlib-jdk8", kotlinVersion))
     implementation(kotlin("serialization", kotlinVersion))
-    implementation("com.squareup:kotlinpoet:1.0.0")
-    implementation("org.apache.bcel:bcel:6.0")
     implementation(kotlin("reflect", kotlinVersion))
 }
