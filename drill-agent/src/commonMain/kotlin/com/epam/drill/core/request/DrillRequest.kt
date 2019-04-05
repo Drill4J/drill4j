9147acea11c54128d236e48b60681242b0ab6439
Igor_Kuzminykh <Igor_Kuzminykh@epam.com>
2019-04-05 22:33:23 +0300

    moved DrillRequest to common module

---
+++ drill-agent/src/commonMain/kotlin/com/epam/drill/core/request/DrillRequest.kt
@@ -0,0 +1,70 @@
+package com.epam.drill.core.request
+
+data class DrillRequest(val drillSessionId: String?, val host: String?, val additionalConfig: String?)
+
+typealias RawHttpRequest = String
+
+const val COOKIE_HEADER_NAME = "Cookie"
+
+fun parseHttpRequest(request: RawHttpRequest): HttpRequest {
+    val reader = request.lineSequence()
+    val query = reader.first().toRequestQuery()
+    val requestHeaders = parseHeaders(reader.drop(1))
+    val cookies = parseCookies(requestHeaders)
+    return HttpRequest(query, requestHeaders, cookies)
+
+}
+
+private fun parseCookies(requestHeaders: Map<String, String>) = requestHeaders[COOKIE_HEADER_NAME]?.run {
+    val cookies = mutableMapOf<String, String>()
+    val split = this.trimStart().split("; ").dropLastWhile { it.isBlank() }
+    for (rawCookie in split) {
+        val eqIndex = rawCookie.indexOfFirst { it == '=' }
+        val key = rawCookie.slice(0 until eqIndex)
+        val value = rawCookie.slice(eqIndex + 1 until rawCookie.length)
+        cookies[key] = value
+    }
+    cookies
+} ?: mutableMapOf()
+
+
+private fun parseHeaders(rawHeaders: Sequence<String>) = rawHeaders.filter { it.isNotBlank() }.associate { parseHeaderLine(it) }
+
+private fun parseHeaderLine(header: String): Pair<String, String> {
+    val idx = header.indexOfFirst { it == ':' }
+    if (idx == -1) {
+        throw RuntimeException("Invalid Header Parameter: $header")
+    }
+    return header.substring(0, idx) to header.substring(idx + 2, header.length)
+}
+
+
+data class HttpRequest(
+    val query: Query,
+    val headers: Map<String, String>,
+    val cookies: Map<String, String>
+)
+
+fun HttpRequest.toRawRequestString() =
+    "$query\n" +
+            "${headers.map { it.key + ": " + it.value }.joinToString("\n")}\n" +
+            "$COOKIE_HEADER_NAME:  ${cookies.map { it.key + "=" + it.value }.joinToString("; ")}"
+
+fun HttpRequest.toDrillRequest(): DrillRequest {
+    return DrillRequest(
+        this.headers["DrillSessionId"] ?: this.cookies["DrillSessionId"],
+        this.headers["Host"],
+        this.headers["DrillAdditionalConfig"]
+    )
+}
+
+fun RawHttpRequest.toRequestQuery(): Query {
+    val queryValues = this.split(" ")
+    return Query(queryValues[0], queryValues[1], queryValues[2])
+}
+
+data class Query(val method: String, val url: String, val version: String) {
+    override fun toString(): String {
+        return "$method $url $version"
+    }
+}
