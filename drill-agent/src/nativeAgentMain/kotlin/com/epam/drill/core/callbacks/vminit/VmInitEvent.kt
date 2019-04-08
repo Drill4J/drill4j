8db70955c2251301a436bd0e2fd1d6e008824809
Igor_Kuzminykh <Igor_Kuzminykh@epam.com>
2019-04-08 19:33:33 +0300

    quick fix of pluginLoading priority

--- drill-agent/src/nativeAgentMain/kotlin/com/epam/drill/core/callbacks/vminit/VmInitEvent.kt
+++ drill-agent/src/nativeAgentMain/kotlin/com/epam/drill/core/callbacks/vminit/VmInitEvent.kt
@@ -2,22 +2,17 @@
 
 package com.epam.drill.core.callbacks.vminit
 
+import com.epam.drill.core.di
 import com.epam.drill.core.ws.startWs
 import com.epam.drill.logger.DLogger
+import com.soywiz.klock.DateTime
 import com.soywiz.klogger.Logger
-import com.soywiz.kmem.buildByteArray
-import drillInternal.getLoadedClass
+import com.soywiz.korio.lang.Thread_sleep
 import jvmapi.JNIEnvVar
 import jvmapi.jthread
 import jvmapi.jvmtiEnvVar
-import kotlinx.cinterop.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.cinterop.CPointer
 import kotlinx.coroutines.runBlocking
-import org.objectweb.kn.asm.ClassReader
-import platform.posix.pthread_self
-import kotlin.native.concurrent.TransferMode
-import kotlin.native.concurrent.Worker
 
 
 val initLogger: Logger
@@ -29,43 +24,15 @@ val initLogger: Logger
 fun jvmtiEventVMInitEvent(env: CPointer<jvmtiEnvVar>?, jniEnv: CPointer<JNIEnvVar>?, thread: jthread?) = runBlocking {
     startWs()
     println("WM IS INIT")
-    Worker.start(true).execute(TransferMode.UNSAFE, {}) {
-        runBlocking {
-
-            launch {
-                while (true) {
-                    delay(5)
-//                    getLoadedClass()?.apply {
-//                        try {
-//                            val pointed = this.pointed
-//                            val classDataLen = pointed.size
-//                            val classData = pointed.array!!
-//
-//                            val bytes = buildByteArray {
-//                                for (i in 0 until classDataLen) {
-//                                    append(classData[i].toByte())
-//                                }
-//                            }
-//                            println(bytes)
-//                        }catch (ex:Throwable){
-//                            ex.printStackTrace()
-//                        }
-//
-////                        println(this)
-//                    }
-                }
-            }
+    val timeoutSec = 15
+    val startTime = DateTime.now()
+    val pluginId = "coverage"
+    while (di { pInstrumentedStorage }[pluginId] == null) {
+        if (DateTime.now().minus(startTime).seconds > timeoutSec) {
+            println("Can't loaded the $pluginId plugin in $timeoutSec seconds")
+            break
         }
+        Thread_sleep(1000)
     }
-//    val findClass: jclass? = FindClass("com/epam/drill/instr/ASM")
-//    println(findClass)
-//    val getStaticFieldID: jfieldID? = GetStaticFieldID(findClass, "INSTANCE", "Lcom/epam/drill/instr/ASM;")
-//    val getStaticObjectField: jobject? = GetStaticObjectField(findClass, getStaticFieldID)
-//    val getMethodID: jmethodID? = GetMethodID(findClass, "emulate", "()[B")
-////
-//    di.singleton(findClass!! as Any)
-//    di.singleton(getStaticObjectField!! as Any)
-//    di.singleton(getMethodID!! as Any)
-    println(pthread_self())
 }
 
