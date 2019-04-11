6315b79fd286336b5204abbc5e9c40d1d44d6108
Denis Domashenko <denis_domashenko@epam.com>
2019-04-11 14:09:43 +0300

    Added build version's history saving

--- drill-admin/src/adminMain/kotlin/com/epam/drill/endpoints/ServerWsTopics.kt
+++ drill-admin/src/adminMain/kotlin/com/epam/drill/endpoints/ServerWsTopics.kt
@@ -5,16 +5,19 @@ import com.epam.drill.agentmanager.AgentStorage
 import com.epam.drill.agentmanager.byId
 import com.epam.drill.agentmanager.toAgentInfoWebSocket
 import com.epam.drill.agentmanager.toAgentInfosWebSocket
+import com.epam.drill.common.AgentBuildVersion
 import com.epam.drill.common.Message
 import com.epam.drill.common.MessageType
 import com.epam.drill.plugins.Plugins
 import com.epam.drill.plugins.toAllPluginsWebSocket
 import com.epam.drill.router.WsRoutes
+import com.epam.drill.storage.MongoClient
 import io.ktor.application.Application
 import kotlinx.coroutines.runBlocking
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.generic.instance
+import org.litote.kmongo.getCollection
 
 
 class ServerWsTopics(override val kodein: Kodein) : KodeinAware {
@@ -22,6 +25,7 @@ class ServerWsTopics(override val kodein: Kodein) : KodeinAware {
     private val agentStorage: AgentStorage by instance()
     private val plugins: Plugins by instance()
     private val app: Application by instance()
+    private val mc: MongoClient by instance()
     private val sessionStorage: MutableSet<DrillWsSession> by instance()
 
     init {
@@ -50,6 +54,17 @@ class ServerWsTopics(override val kodein: Kodein) : KodeinAware {
 
                 topic<WsRoutes.GetAgent> { x, _ ->
                     agentStorage.byId(x.agentId)?.toAgentInfoWebSocket()
+                }
+
+                topic<WsRoutes.GetAgentBuilds> { agent,_ ->
+                    println(agent)
+                    val collection = mc.client!!
+                        .getDatabase(agentStorage.byId(agent.agentId)?.id)
+                        .getCollection<AgentBuildVersion>("agent-build-versions")
+                    val versions = collection.find().toList()
+                    println(versions.stringify())
+                    if (versions.isEmpty()) null
+                    else versions
                 }
 
                 topic<WsRoutes.GetAllPlugins> { _, _ ->
