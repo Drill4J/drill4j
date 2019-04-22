06468de5fe86b9d3ea9c71c10b5e16e5f8bd3850
Viktor_Bellavin <viktor_bellavin@epam.com>
2019-04-22 19:29:22 +0300

    US 10.6 Associated tests: added package/class/method info to assoc tests.

--- plugins/drill-coverage-plugin/src/adminPartMain/kotlin/JacocoExt.kt
+++ plugins/drill-coverage-plugin/src/adminPartMain/kotlin/JacocoExt.kt
@@ -1,7 +1,22 @@
 package com.epam.drill.plugins.coverage
 
+import org.jacoco.core.analysis.IClassCoverage
 import org.jacoco.core.analysis.ICoverageNode
+import org.jacoco.core.analysis.IMethodCoverage
+import org.jacoco.core.analysis.IPackageCoverage
 import org.jacoco.core.internal.data.CRC64
+
+data class CoverageKey(
+    val id: String,
+    val packageName: String? = null,
+    val className: String? = null,
+    val methodName: String? = null,
+    val methodDesc: String? = null
+) {
+    override fun equals(other: Any?) = other is CoverageKey && id == other.id
+
+    override fun hashCode() = id.hashCode()
+}
 
 val String.crc64: String get() = CRC64.classId(toByteArray()).toString(Character.MAX_RADIX)
 
@@ -10,3 +25,23 @@ val ICoverageNode.coverage: Double?
         val ratio = this.instructionCounter.coveredRatio
         return if (ratio.isFinite()) ratio * 100.0 else null
     }
+
+fun ICoverageNode.coverageKey(parent: ICoverageNode? = null): CoverageKey = when (this) {
+    is IMethodCoverage -> CoverageKey(
+        id = "${parent?.name}.${this.name}${this.desc}".crc64,
+        packageName = parent?.name?.substringBeforeLast('/'),
+        className = parent?.name,
+        methodName = this.name,
+        methodDesc = this.desc
+    )
+    is IClassCoverage -> CoverageKey(
+        id = this.name.crc64,
+        packageName = this.name.substringBeforeLast('/'),
+        className = this.name
+    )
+    is IPackageCoverage -> CoverageKey(
+        id = this.name.crc64,
+        packageName = this.name
+    )
+    else -> CoverageKey(this.name.crc64)
+}
