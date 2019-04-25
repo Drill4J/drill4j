122b9050c716744a21148f53717c1862b45c5029
Viktor_Bellavin <viktor_bellavin@epam.com>
2019-04-25 12:09:19 +0300

    Minor: corrected parameter name AdminPluginPart#processData - agentId -> agentInfo

--- drill-plugin-api/drill-admin-part/src/drillAdminPartMain/kotlin/com/epam/drill/plugin/api/end/AdminPluginPart.kt
+++ drill-plugin-api/drill-admin-part/src/drillAdminPartMain/kotlin/com/epam/drill/plugin/api/end/AdminPluginPart.kt
@@ -3,9 +3,7 @@ package com.epam.drill.plugin.api.end
 import com.epam.drill.common.AgentInfo
 import com.epam.drill.plugin.api.DrillPlugin
 import com.epam.drill.plugin.api.message.DrillMessage
-import java.util.concurrent.ConcurrentHashMap
 
 abstract class AdminPluginPart(val sender: WsService, override val id: String) : DrillPlugin() {
-    abstract suspend fun processData(agentId: AgentInfo, dm: DrillMessage): Any
-
+    abstract suspend fun processData(agentInfo: AgentInfo, dm: DrillMessage): Any
 }
