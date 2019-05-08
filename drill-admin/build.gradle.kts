48cc658caa12d5692084f44e250c6b2ea2b96b85
Viktor_Bellavin <viktor_bellavin@epam.com>
2019-05-08 20:56:14 +0300

    Improvements in gradle build of the drill-admin module:  - replaced kotlin multiplatform with kotlin jvm plugin;  - removed local kodein libs, updated kodein version (6.1 -> 6.2)

--- drill-admin/build.gradle.kts
+++ drill-admin/build.gradle.kts
@@ -1,96 +1,71 @@
-import com.epam.drill.build.serializationRuntimeVersion
 import com.epam.drill.build.ktorVersion
+import com.epam.drill.build.serializationRuntimeVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("kotlin-multiplatform")
+    kotlin("jvm")
     id("kotlinx-serialization")
+    application
 }
 
 repositories {
     mavenCentral()
-    mavenLocal()
     jcenter()
-    maven(url = "https://dl.bintray.com/kodein-framework/Kodein-DI")
-    maven(url = "https://dl.bintray.com/soywiz/soywiz")
-    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
-    maven(url = "https://kotlin.bintray.com/ktor")
-    maven(url = "https://dl.bintray.com/spekframework/spek-dev")
-    maven(url = "https://kotlin.bintray.com/kotlinx")
-    maven(url = "https://mymavenrepo.com/repo/OgSYgOfB6MOBdJw3tWuX/")
+    maven(url = "https://dl.bintray.com/kodein-framework/Kodein-DI/")
+    mavenLocal()
 }
 
-kotlin {
-
-    targets {
-        jvm("admin")
-    }
-
-    sourceSets {
-        jvm("admin").compilations["main"].defaultSourceSet {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializationRuntimeVersion")
-                implementation("org.litote.kmongo:kmongo:3.9.0")
-                implementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:2.1.1")
-                implementation("io.ktor:ktor-auth:$ktorVersion")
-                implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
-                //fixme temp solution
-                implementation(fileTree(file("testLib/kodein-di-generic-jvm-6.0.1.jar")))
-                implementation(fileTree(file("testLib/kodein-di-core-jvm-6.0.1.jar")))
-                implementation("io.ktor:ktor-server-netty:$ktorVersion")
-                implementation("io.ktor:ktor-locations:$ktorVersion")
-                implementation("io.ktor:ktor-gson:$ktorVersion")
-                implementation("io.ktor:ktor-server-core:$ktorVersion")
-                implementation("io.ktor:ktor-websockets:$ktorVersion")
-                implementation("io.ktor:ktor-html-builder:$ktorVersion")
-                implementation("ch.qos.logback:logback-classic:1.2.1")
-                implementation("ch.qos.logback:logback-classic:1.2.1")
-                implementation(project(":drill-common"))
-                implementation(project(":drill-plugin-api:drill-admin-part"))
-
-            }
-        }
-
-        jvm("admin").compilations["test"].defaultSourceSet {
-            dependencies {
-                implementation("io.ktor:ktor-server-test-host:$ktorVersion")
-                implementation("org.testcontainers:testcontainers:1.11.1")
-            }
-        }
+val appMainClassName = "io.ktor.server.netty.EngineMain"
 
 
-    }
+application {
+    mainClassName = appMainClassName
+
+    applicationDefaultJvmArgs = listOf(
+        "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006",
+        "-Djava.awt.headless=true",
+        "-Xms128m",
+        "-Xmx2g",
+        "-XX:+UseG1GC",
+        "-XX:MaxGCPauseMillis=100"
+    )
+}
+
+
+dependencies {
+    implementation(project(":drill-common"))
+    implementation(project(":drill-plugin-api:drill-admin-part"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializationRuntimeVersion")
+    implementation("org.litote.kmongo:kmongo:3.9.0")
+    implementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:2.1.1")
+    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
+    implementation("org.kodein.di:kodein-di-generic-jvm:6.2.0")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-locations:$ktorVersion")
+    implementation("io.ktor:ktor-gson:$ktorVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-websockets:$ktorVersion")
+    implementation("io.ktor:ktor-html-builder:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:1.2.1")
+    implementation("ch.qos.logback:logback-classic:1.2.1")
+
+
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation("org.testcontainers:testcontainers:1.11.1")
 }
 
 tasks {
-
-
-    val adminJar = "adminJar"(Jar::class) {
-        manifest {
-            attributes(mapOf("Main-Class" to "io.ktor.server.netty.EngineMain"))
-        }
-
-        from(provider {
-            kotlin.targets["admin"].compilations["main"].compileDependencyFiles.map {
-                if (it.isDirectory) it else zipTree(it)
-            }
-        })
-
-        archiveFileName.set("drillAdmin.jar")
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
     }
+
+    val run by existing(JavaExec::class)
+
+    //TODO Only for backward compatibility, remove after CI/CD has been configured
     register("runDrillAdmin") {
         group = "application"
-        dependsOn(adminJar)
-        doLast {
-            javaexec {
-                classpath = (kotlin.targets["admin"].compilations["main"].compileDependencyFiles)
-                classpath(adminJar)
-                main = "io.ktor.server.netty.EngineMain"
-                jvmArgs(
-                    "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006",
-                    "-Xmx2g"
-                )
-            }
-        }
-
+        dependsOn(run)
     }
 }
