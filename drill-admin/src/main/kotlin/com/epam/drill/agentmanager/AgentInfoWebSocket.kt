e515bd3130898fa48b72d3d0d4943babc0d83dba
s0larm00n <s0larm00n@ya.ru>
2019-05-19 22:10:47 +0400

    D4J-77. Added limitation for id as well

--- drill-admin/src/main/kotlin/com/epam/drill/agentmanager/AgentInfoWebSocket.kt
+++ drill-admin/src/main/kotlin/com/epam/drill/agentmanager/AgentInfoWebSocket.kt
@@ -73,7 +73,11 @@ fun MutableSet<PluginBean>.activePluginsCount() = this.count { it.enabled }
 fun MutableSet<AgentInfo>.toAgentInfosWebSocket() = this.map {
     it.run {
         AgentInfoWebSocket(
-            id = id,
+            id = if(id!= null && id.length > 20){
+                println("Agent id was too long for agent ${name}," +
+                        "had to shorten it to 20 symbols")
+                id.substring(0,20)
+            } else id,
             name = name,
             description = if(description!= null && description.length > 200){
                 println("Agent description was too long for agent ${name}," +
