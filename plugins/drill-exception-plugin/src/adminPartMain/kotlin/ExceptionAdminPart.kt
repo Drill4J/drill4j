c639f4788c5f1a71c440b66e9b8f952da26dde95
Kristina Smirnova <kristina_smirnova@epam.com>
2019-06-21 11:15:43 +0300

    EPMDJ-280. Fixed ExceptionAdminPart realization to remove conflicts with EPMDJ-43 commits

--- plugins/drill-exception-plugin/src/adminPartMain/kotlin/ExceptionAdminPart.kt
+++ plugins/drill-exception-plugin/src/adminPartMain/kotlin/ExceptionAdminPart.kt
@@ -4,11 +4,17 @@ import com.epam.drill.common.AgentInfo
 import com.epam.drill.plugin.api.end.AdminPluginPart
 import com.epam.drill.plugin.api.end.WsService
 import com.epam.drill.plugin.api.message.DrillMessage
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.serializer
 
 
 @Suppress("unused")
-class ExceptionAdminPart(private val ws: WsService, id: String) : AdminPluginPart(ws, id) {
+class ExceptionAdminPart(private val ws: WsService, id: String) : AdminPluginPart<String>(ws, id) {
 
+    override var actionSerializer: KSerializer<String> = String.serializer()
+
+    override suspend fun doAction(agentInfo: AgentInfo, action: String) {
+    }
 
     override suspend fun processData(agentInfo: AgentInfo, dm: DrillMessage): Any {
         println("$id got a message ${dm.content}")
