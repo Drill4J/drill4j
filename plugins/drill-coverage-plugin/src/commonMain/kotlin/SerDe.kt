32008aedc5fc900491d0727f6e81652ea25dc502
Kristina Smirnova <kristina_smirnova@epam.com>
2019-07-18 11:59:03 +0300

    EPMDJ-364. Added RenameActiveScope serializer to Action polymorphic

--- plugins/drill-coverage-plugin/src/commonMain/kotlin/SerDe.kt
+++ plugins/drill-coverage-plugin/src/commonMain/kotlin/SerDe.kt
@@ -7,6 +7,7 @@ import kotlinx.serialization.modules.*
 private val serialModule = SerializersModule {
     polymorphic<Action> {
         addSubclass(SwitchActiveScope.serializer())
+        addSubclass(RenameActiveScope.serializer())
         addSubclass(ToggleScope.serializer())
         addSubclass(DropScope.serializer())
 
